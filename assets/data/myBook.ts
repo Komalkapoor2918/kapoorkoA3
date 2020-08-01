7949b9ca3edd53fd6d9ce4c701a7e1efcd121518
Komalkapoor2918 <55361853+Komalkapoor2918@users.noreply.github.com>
2020-07-31 23:44:07 -0400

    Add files via upload

---
+++ assets/data/myBook.ts
@@ -0,0 +1,11 @@
+
+import { Book } from '../../app/bookInfo';
+
+export const BOOKS : Book[] =[
+{bookName :'Rich Dad', author :'Kim', genre : 'Literature', yearPublished :'2010' },
+{bookName :'Good things', author :'John', genre : 'Drama', yearPublished :'2013' },
+{bookName :'Never Die', author :'Dr Bose', genre : 'Comedy', yearPublished :'2015' },
+{bookName :'Truth Wins', author :'Ted', genre : 'Drama', yearPublished :'2018' }
+
+
+];
